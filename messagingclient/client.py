f7c7e7aa9279e07dd0891d45de028bd0a9bdef0f
Keith Wiley <keith.wiley@alleninstitute.org>
2025-03-20 10:04:26 -0700

    Added subscription and delivery attempts to message prior to callback.

--- messagingclient/client.py
+++ messagingclient/client.py
@@ -39,6 +39,14 @@ class MessagingClient:
             logging.info(
                 f"MessagingClient.consume().callback_wrapper() Received message: {payload}."
             )
+            try:
+                payload.message.attributes["__subscription_name"] = queue
+            except Exception as e:
+                print("MessagingClient.consume().callback_wrapper() Error adding subscription_name: ", e)
+            try:
+                payload.message.attributes["__delivery_attempt"] = payload.delivery_attempt
+            except Exception as e:
+                print("MessagingClient.consume().callback_wrapper() Error adding delivery_attempt: ", e)
             callback(payload)
             payload.ack()
 
@@ -90,6 +98,14 @@ class MessagingClient:
                     logging.info(
                         f"MessagingClient.consume_multiple() Received message on subscription '{subscription_name}': {received_message.message.data}."
                     )
+                    try:
+                        received_message.message.attributes["__subscription_name"] = subscription_name
+                    except Exception as e:
+                        print("MessagingClient._consume_round_robin() Error adding subscription_name: ", e)
+                    try:
+                        received_message.message.attributes["__delivery_attempt"] = received_message.delivery_attempt
+                    except Exception as e:
+                        print("MessagingClient._consume_round_robin() Error adding delivery_attempt: ", e)
                     callback(received_message.message)
                     ack_ids.append(received_message.ack_id)
                 except Exception as exc:
